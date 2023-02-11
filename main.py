1af2aa3bc978955ca7597f878d79ceb610d2f884
EndovelicoPT <lmcabral.silva@gmail.com>
2023-02-11 18:19:04 +0000

    Tidy up the code

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from tkinter import *
-from tkinter import filedialog 
+from tkinter import filedialog
 from PIL import Image, ImageDraw, ImageFont
 
-
+# Create window
 window = Tk()
 window.title("Watermarker")
 window.config(padx=50, pady=10)
 
+# Pack window with logo and labels
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="camera.png")
 logo = Label(image=logo_img)
@@ -16,10 +17,12 @@ title.grid(row=2)
 custom_watermark = Entry(window, justify='center')
 custom_watermark.grid(row=3)
 
+
 def uploadImg():
     global image
-    filename = filedialog.askopenfilename(initialdir =  "/", title = "Select an Image", filetype = (("jpeg files","*.jpg"),("PNG  files","*.png")))
-    image = Image.open(filename) # Read the Image
+    filename = filedialog.askopenfilename(
+        initialdir="/", title="Select an Image", filetype=(("jpeg files", "*.jpg"), ("PNG  files", "*.png")))
+    image = Image.open(filename)
     w, h = image.size
     x, y = int(w / 2), int(h / 2)
     if x > y:
@@ -29,23 +32,25 @@ def uploadImg():
     else:
         font_size = x
     font = ImageFont.truetype("arial.ttf", int(font_size/20))
-    # make the image editable
     draw = ImageDraw.Draw(image)
+
+    # Add watermark
     draw.text((0, 0), custom_watermark.get(), fill=(0, 0, 0), font=font)
     image.show()
-  
+
 
 def savefile():
     files = [('JPG Files', '*.jpg'),
              ('PNG Files', '*.png')]
-    filename = filedialog.asksaveasfile(filetypes=files, defaultextension=files)
+    filename = filedialog.asksaveasfile(
+        filetypes=files, defaultextension=files)
     image.save(filename)
 
-btn_upload_img = Button(text="Upload Image", bg="white", command= uploadImg)
-btn_upload_img.grid(row=4, column=0)
-btn_save = Button( text="Save", bg="white", command=savefile)
-btn_save.grid(row=5, column=0)
 
+btn_upload_img = Button(text="Upload Image", bg="white", command=uploadImg)
+btn_upload_img.grid(row=4, column=0)
+btn_save = Button(text="Save", bg="white", command=savefile)
+btn_save.grid(row=5, column=0)
 
 
 window.mainloop()
